4933a205bb7390ab7a1a3fc368aea2da0b913da0
MithoonSharechat <mithoon.sharechat.co>
2019-05-14 15:45:16 +0530

    Refractor

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let loggerLib = require(__dirname + '/utils/logger.js');
 let logger = loggerLib.logger({ indexName: "onboarding-test-kibana-index" }).logger;
-console.log("Hello Folks");
 logger.log({
     level: 'info',
     message: "Publishing log using winston to kibana"
--- config/elasticSearchConfig.js
+++ config/elasticSearchConfig.js
@@ -4,10 +4,6 @@ var config = {
     "maxLogFileSize": 1000000,
     "intervalBetweenCalls": 5000,
     "esHost": "https://vpc-log-dump-pfy2smttiqzrcwbag6nqndo42u.ap-south-1.es.amazonaws.com",
-    "microServiceBaseUrl": 'https://dev-kube-api.sckops.com/',
-    "user_service": "http://user-service.sckops.com/user-service",
-    "account_service": "http://account-service.sckops.com/account-service",
-    "x_sharechat_authorized_userId": "X-SHARECHAT-AUTHORIZED-USERID"
 }
 
 module.exports = config;
